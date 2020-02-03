d88c104c9ea17d94096be474c7744fc725ca684e
志豪 黄 <zero375211607@163.com>
2020-02-03 17:48:45 +0800

    增加注释

--- school sport/method.cpp
+++ school sport/method.cpp
@@ -2,8 +2,8 @@
 #include<stdio.h>
 #include"method.h"
 
+//若entry在mix与max之间,则返回int型的entry,否则,返回0
 int entrycheck(char entry,int mix,int max) {
-
 	if (48 + mix <= entry&&entry <= 48 + max)
 		return entry - 48;
 	else return 0;
