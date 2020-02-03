6d1b166a38abdc2e7c9d119fc65fa27f7953f737
志豪 黄 <zero375211607@163.com>
2020-02-03 14:24:59 +0800

    main.cpp ,增加主分支选项

--- school sport/main.cpp
+++ school sport/main.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
+#include<stdio.h>
+
+void esinforma() {
+	int a;
+	system("CLS");
+	printf("1.单位信息\n");
+	printf("2.运动员信息\n");
+	printf("3.项目信息\n");
+
+	scanf_s("%d", &a);
+
+	switch (a) {
+	case 1: break;
+	case 2: break;
+	case 3: break;
+	}
+}
 
 void main() {
-	printf("hellow world!!!123\n");
-	system("pause");
+	int a;
+	system("CLS");
+	printf("1.基本信息管理\n");
+	printf("2.成绩录入管理\n");
+	printf("3.冬季查询管理\n");
+
+	scanf_s("%d", &a);
+
+	switch (a) {
+	case 1: esinforma(); break;
+	case 2: break;
+	case 3: break;
+	}
 }
+
+
